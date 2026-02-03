d21c1463bb311cc593d927def02379496a03bbc7
SkyLestor <timur_skocylov@outlook.com>
2026-02-03 16:24:58 +0200

    SHT-8 Implement Database Seeders and Factories

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Movie extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'title',
         'is_published',
