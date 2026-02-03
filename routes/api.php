6a69adbb648161e76ac0c410ad5295f3d06571dc
SkyLestor <timur_skocylov@outlook.com>
2026-02-03 19:04:19 +0200

    SHT-16 Implement Admin CRUD Controllers

--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,26 @@ use App\Http\Controllers\Api\GenreController;
 use App\Http\Controllers\Api\MovieController;
 use Illuminate\Support\Facades\Route;
 
-// index -> GET /api/genres
-// show  -> GET /api/genres/{id}
+/**
+ * index -> GET /api/genres
+ * show  -> GET /api/genres/{id}
+ */
 Route::apiResource('genres', GenreController::class)
     ->only(['index', 'show']);
 
 
-// index -> GET /api/movies
-// show  -> GET /api/movies/{id}
-Route::apiResource('movies', MovieController::class)
-    ->only(['index', 'show']);
+/**
+ * Movie CRUD Routes
+ * index   -> GET /api/movies
+ * store   -> POST /api/movies
+ * show    -> GET /api/movies/{id}
+ * update  -> PUT/PATCH /api/movies/{id}
+ * destroy -> DELETE /api/movies/{id}
+ */
+Route::apiResource('movies', MovieController::class);
+
+/**
+ * Custom Route: Publish/Unpublish a movie
+ * publish -> PATCH /api/movies/{id}/publish
+ */
+Route::patch('movies/{movie}/publish', [MovieController::class, 'publish']);
